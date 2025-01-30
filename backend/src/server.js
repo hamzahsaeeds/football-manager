2266bf2fe2bd712c74e912e18fd6acbd769c207c
hamzahsaeeds <hamzahsaeed23@hotmail.com>
2025-01-30 15:26:52 +0500

    added cors on backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const app = require("./app");
+const cors = require("cors");
 
 dotenv.config();
 connectDB();
+
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
